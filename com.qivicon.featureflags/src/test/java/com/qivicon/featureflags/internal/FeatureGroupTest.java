b3ab162f604c4bbf75e794c4b15329318aeb1295
Amit Kumar Mondal <admin@amitinside.com>
2017-10-14 05:34:01 +0200

    Added skeletal tests and test dependency

---
+++ com.qivicon.featureflags/src/test/java/com/qivicon/featureflags/internal/FeatureGroupTest.java
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2017 QIVICON
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Amit Kumar Mondal
+ *
+ *******************************************************************************/
+package com.qivicon.featureflags.internal;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+public final class FeatureGroupTest {
+
+    @Test
+    public void testA() {
+        final FeatureManager f = new FeatureManager();
+        assertNotNull(f);
+    }
+
+}
