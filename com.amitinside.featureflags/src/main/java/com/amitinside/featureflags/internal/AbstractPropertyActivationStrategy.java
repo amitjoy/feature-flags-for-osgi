8025212bda22c9175ae4c626de01a2b748c1d44c
Amit Kumar Mondal <admin@amitinside.com>
2017-10-20 07:34:04 +0200

    Extracted Property Strategy to Abstract Class for extension

---
+++ com.amitinside.featureflags/src/main/java/com/amitinside/featureflags/internal/AbstractPropertyActivationStrategy.java
@@ -0,0 +1,95 @@
+/*******************************************************************************
+ * Copyright (c) 2017 Amit Kumar Mondal
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ *******************************************************************************/
+package com.amitinside.featureflags.internal;
+
+import static com.amitinside.featureflags.internal.Config.*;
+import static com.google.common.base.Preconditions.checkArgument;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import com.amitinside.featureflags.strategy.ActivationStrategy;
+import com.amitinside.featureflags.util.ConfigHelper;
+import com.google.common.base.Objects;
+import com.google.common.base.Strings;
+
+public abstract class AbstractPropertyActivationStrategy implements ActivationStrategy {
+
+    private String name;
+    private String description;
+    private String propertyKey;
+    private String propertyValue;
+
+    private final Lock lock = new ReentrantLock(true);
+
+    protected void activate(final Map<String, Object> properties) {
+        extractProperties(properties);
+    }
+
+    protected void updated(final Map<String, Object> properties) {
+        extractProperties(properties);
+    }
+
+    private void extractProperties(final Map<String, Object> properties) {
+        lock.lock();
+        try {
+            final Map<Config, Object> props = ConfigHelper.parseProperties(properties);
+
+            name = (String) props.get(NAME);
+            checkArgument(!Strings.isNullOrEmpty(name), "Strategy name cannot be null or empty");
+
+            //@formatter:off
+            description = Optional.ofNullable(props.get(DESCRIPTION)).map(String.class::cast)
+                                                                     .orElse(name);
+            propertyKey = Optional.ofNullable(props.get(PROPERTY_KEY)).map(String.class::cast)
+                                                               .filter(s -> !s.isEmpty())
+                                                               .orElse(null);
+            propertyValue = Optional.ofNullable(props.get(PROPERTY_VALUE)).map(String.class::cast)
+                                                               .filter(s -> !s.isEmpty())
+                                                               .orElse(null);
+            //@formatter:on
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public Optional<String> getDescription() {
+        return Optional.of(description);
+    }
+
+    public Optional<String> getKey() {
+        return Optional.ofNullable(propertyKey);
+    }
+
+    public Optional<String> getValue() {
+        return Optional.ofNullable(propertyValue);
+    }
+
+    @Override
+    public String toString() {
+        //@formatter:off
+        return Objects.toStringHelper(this)
+                        .add("Name", name)
+                        .add("Description", description)
+                        .add("Property Key", propertyKey)
+                        .add("Property Value", propertyValue)
+                        .toString();
+        //@formatter:on
+    }
+
+}
--- com.amitinside.featureflags/src/main/java/com/amitinside/featureflags/internal/ServicePropertyActivationStrategy.java
+++ com.amitinside.featureflags/src/main/java/com/amitinside/featureflags/internal/ServicePropertyActivationStrategy.java
@@ -10,99 +10,44 @@
 package com.amitinside.featureflags.internal;
 
 import static com.amitinside.featureflags.Constants.STRATEGY_SYSTEM_PROPERTY_PID;
-import static com.amitinside.featureflags.internal.Config.*;
-import static com.google.common.base.Preconditions.checkArgument;
 import static org.osgi.service.component.annotations.ConfigurationPolicy.REQUIRE;
 
 import java.util.Map;
-import java.util.Optional;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.Objects;
 
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Modified;
 
 import com.amitinside.featureflags.Strategizable;
-import com.amitinside.featureflags.strategy.ActivationStrategy;
-import com.amitinside.featureflags.util.ConfigHelper;
-import com.google.common.base.Objects;
-import com.google.common.base.Strings;
 
-@Component(name = "ConfiguredSystemPropertyStrategy", immediate = true, configurationPolicy = REQUIRE, configurationPid = STRATEGY_SYSTEM_PROPERTY_PID)
-public final class ServicePropertyActivationStrategy implements ActivationStrategy {
+@Component(name = "ConfiguredServicePropertyStrategy", immediate = true, configurationPolicy = REQUIRE, configurationPid = STRATEGY_SYSTEM_PROPERTY_PID)
+public final class ServicePropertyActivationStrategy extends AbstractPropertyActivationStrategy {
 
-    private String name;
-    private String description;
-    private String propertyKey;
-    private String propertyValue;
-
-    private final Lock lock = new ReentrantLock(true);
-
+    @Override
     @Activate
     protected void activate(final Map<String, Object> properties) {
-        extractProperties(properties);
+        super.activate(properties);
     }
 
+    @Override
     @Modified
     protected void updated(final Map<String, Object> properties) {
-        extractProperties(properties);
-    }
-
-    private void extractProperties(final Map<String, Object> properties) {
-        lock.lock();
-        try {
-            final Map<Config, Object> props = ConfigHelper.parseProperties(properties);
-
-            name = (String) props.get(NAME);
-            checkArgument(!Strings.isNullOrEmpty(name), "Strategy name cannot be null or empty");
-
-            //@formatter:off
-            description = Optional.ofNullable(props.get(DESCRIPTION)).map(String.class::cast)
-                                                                     .orElse(name);
-            propertyKey = Optional.ofNullable(props.get(PROPERTY_KEY)).map(String.class::cast)
-                                                               .filter(s -> !s.isEmpty())
-                                                               .orElse(null);
-            propertyValue = Optional.ofNullable(props.get(PROPERTY_VALUE)).map(String.class::cast)
-                                                               .filter(s -> !s.isEmpty())
-                                                               .orElse(null);
-            //@formatter:on
-        } finally {
-            lock.unlock();
-        }
-    }
-
-    @Override
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public Optional<String> getDescription() {
-        return Optional.of(description);
+        super.updated(properties);
     }
 
     @Override
     public boolean isEnabled(final Strategizable strategizable, final Map<String, Object> properties) {
-        if (propertyKey == null || propertyValue == null) {
+        final String key = getKey().orElse(null);
+        final String value = getValue().orElse(null);
+
+        if (key == null || value == null) {
             return false;
         }
-        if (properties.containsKey(propertyKey)) {
-            return Objects.equal(properties.get(propertyKey), propertyValue);
+        if (properties.containsKey(key)) {
+            return Objects.equals(properties.get(key), value);
         }
         return false;
-    }
-
-    @Override
-    public String toString() {
-        //@formatter:off
-        return Objects.toStringHelper(this)
-                        .add("Name", name)
-                        .add("Description", description)
-                        .add("Property Key", propertyKey)
-                        .add("Property Value", propertyValue)
-                        .toString();
-        //@formatter:on
     }
 
 }
