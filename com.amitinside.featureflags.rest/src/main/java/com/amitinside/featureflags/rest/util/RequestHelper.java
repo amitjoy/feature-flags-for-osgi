32205ebcfa2cde4d089bd0b84cfaa6770b9307f5
Amit Kumar Mondal <admin@amitinside.com>
2017-11-04 00:00:40 +0100

    Removed Unnecessary Dependencies and Methods

--- com.amitinside.featureflags.rest/src/main/java/com/amitinside/featureflags/rest/util/RequestHelper.java
+++ com.amitinside.featureflags.rest/src/main/java/com/amitinside/featureflags/rest/util/RequestHelper.java
@@ -9,19 +9,9 @@
  *******************************************************************************/
 package com.amitinside.featureflags.rest.util;
 
-import static java.nio.charset.StandardCharsets.UTF_8;
-import static java.util.Objects.requireNonNull;
-
-import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
-import java.util.Enumeration;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
 
 import org.osgi.dto.DTO;
 import org.osgi.framework.BundleContext;
@@ -32,7 +22,6 @@ import com.amitinside.featureflags.feature.Feature;
 import com.amitinside.featureflags.feature.group.FeatureGroup;
 import com.amitinside.featureflags.strategy.ActivationStrategy;
 import com.amitinside.featureflags.util.ServiceHelper;
-import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 
 import osgi.enroute.rest.api.RESTRequest;
@@ -44,69 +33,6 @@ public final class RequestHelper {
     /** Constructor */
     private RequestHelper() {
         throw new IllegalAccessError("Non-Instantiable");
-    }
-
-    /**
-     * This exists because HttpServletRequest.getParameterMap() returns an String[] instead
-     */
-    public static Map<String, String> getParameterMap(final HttpServletRequest request) {
-        requireNonNull(request, "Servlet Request cannot be null");
-        final Map<String, String> m = new LinkedHashMap<>();
-        for (final Enumeration<?> e = request.getParameterNames(); e.hasMoreElements();) {
-            final String name = (String) e.nextElement();
-            final String value = request.getParameter(name);
-            m.put(name, value);
-        }
-        return m;
-    }
-
-    public static Map<String, String> getHeaderMap(final HttpServletRequest request) {
-        requireNonNull(request, "Servlet Request cannot be null");
-        final Map<String, String> m = new LinkedHashMap<>();
-        for (final Enumeration<?> e = request.getHeaderNames(); e.hasMoreElements();) {
-            final String name = (String) e.nextElement();
-            final String value = request.getHeader(name);
-            m.put(name, value);
-        }
-        return m;
-    }
-
-    public static Map<String, String> getCookieMap(final HttpServletRequest request) {
-        requireNonNull(request, "Servlet Request cannot be null");
-        final Map<String, String> m = new LinkedHashMap<>();
-        final Cookie[] ca = request.getCookies();
-        if (ca != null) {
-            for (final Cookie c : ca) {
-                final String name = c.getName();
-                final String value = c.getValue();
-                m.put(name, value);
-            }
-        }
-        return m;
-    }
-
-    public static List<String> parseFullUrl(final HttpServletRequest request) {
-        requireNonNull(request, "Servlet Request cannot be null");
-        final List<String> parts = Lists.newArrayList();
-        final String pathInfo = request.getRequestURI().substring(request.getContextPath().length() + 1);
-        for (final String val : pathInfo.split("/")) {
-            try {
-                parts.add(URLDecoder.decode(val, UTF_8.name()));
-            } catch (final UnsupportedEncodingException e) {
-                return parts;
-            }
-        }
-        final String query = request.getQueryString();
-        if (query != null) {
-            for (final String val : query.split("&")) {
-                try {
-                    parts.add(URLDecoder.decode(val, UTF_8.name()));
-                } catch (final UnsupportedEncodingException e) {
-                    return parts;
-                }
-            }
-        }
-        return parts;
     }
 
     /**
