00b5d6224427bd65248619feb85fe288dd9c5d88
Amit Kumar Mondal <admin@amitinside.com>
2017-11-09 23:55:02 +0100

    Example Simplified

--- com.amitinside.featureflags.example/src/main/java/com/amitinside/featureflags/example/ExampleFeatureFlag.java
+++ com.amitinside.featureflags.example/src/main/java/com/amitinside/featureflags/example/ExampleFeatureFlag.java
@@ -9,23 +9,14 @@
  *******************************************************************************/
 package com.amitinside.featureflags.example;
 
-import static javax.servlet.http.HttpServletResponse.*;
-
-import java.io.IOException;
 import java.util.Map;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Modified;
-import org.osgi.service.component.annotations.Reference;
-import org.osgi.service.http.HttpService;
-import org.osgi.service.http.NamespaceException;
 import org.osgi.service.metatype.annotations.AttributeDefinition;
 import org.osgi.service.metatype.annotations.Designate;
 import org.osgi.service.metatype.annotations.ObjectClassDefinition;
@@ -35,14 +26,10 @@ import com.amitinside.featureflags.util.Configurable;
 
 @Designate(ocd = MyConfig.class)
 @Component(name = "ExampleFeatureFlag", immediate = true)
-public final class ExampleFeatureFlag extends HttpServlet {
+public final class ExampleFeatureFlag {
 
-    private static final String ALIAS = "/exampleflag";
-
-    private static final long serialVersionUID = 6674752488720831279L;
-
-    private HttpService httpService;
     private MyConfig config;
+    private final ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
 
     @ObjectClassDefinition(id = "feature.flag.example")
     @interface MyConfig {
@@ -50,58 +37,26 @@ public final class ExampleFeatureFlag extends HttpServlet {
         boolean osgi_feature_myfeature() default true;
     }
 
-    /**
-     * Component activation callback
-     */
     @Activate
     protected void activate(final Map<String, Object> properties) {
         modified(properties);
-        try {
-            httpService.registerServlet(ALIAS, this, null, new DisableAuthenticationHttpContext());
-        } catch (ServletException | NamespaceException e) {
-            // not required
-        }
+        doPeriodicStuff();
     }
 
-    /**
-     * Component activation callback
-     */
     @Modified
     protected void modified(final Map<String, Object> properties) {
         config = Configurable.createConfigurable(MyConfig.class, properties);
     }
 
-    /**
-     * Component activation callback
-     */
-    @Deactivate
-    protected void deactivate() {
-        httpService.unregister(ALIAS);
-    }
-
-    /**
-     * {@link HttpService} service binding callback
-     */
-    @Reference
-    protected void setHttpService(final HttpService httpService) {
-        this.httpService = httpService;
-    }
-
-    /**
-     * {@link HttpService} service unbinding callback
-     */
-    protected void unsetHttpService(final HttpService httpService) {
-        this.httpService = null;
-    }
-
-    @Override
-    protected void doGet(final HttpServletRequest req, final HttpServletResponse resp)
-            throws ServletException, IOException {
-        if (config.osgi_feature_myfeature()) {
-            resp.setStatus(SC_OK);
-            return;
-        }
-        resp.setStatus(SC_FORBIDDEN);
+    private void doPeriodicStuff() {
+        executor.scheduleWithFixedDelay(() -> {
+            if (config.osgi_feature_myfeature()) {
+                System.out.println("Example Feature Config >>Enabled<<");
+                return;
+            } else {
+                System.out.println("Example Feature Config >>Disabled<<");
+            }
+        }, 0, 15, TimeUnit.SECONDS);
     }
 
 }
