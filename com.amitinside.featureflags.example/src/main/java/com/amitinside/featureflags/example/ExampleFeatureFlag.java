366d448ae51807a754e19511a1b2bd456effd441
Amit Kumar Mondal <admin@amitinside.com>
2017-11-10 09:52:03 +0100

    Simplified Example Bundle

--- com.amitinside.featureflags.example/src/main/java/com/amitinside/featureflags/example/ExampleFeatureFlag.java
+++ com.amitinside.featureflags.example/src/main/java/com/amitinside/featureflags/example/ExampleFeatureFlag.java
@@ -9,15 +9,8 @@
  *******************************************************************************/
 package com.amitinside.featureflags.example;
 
-import java.util.Map;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.TimeUnit;
-
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Modified;
 import org.osgi.service.metatype.annotations.AttributeDefinition;
 import org.osgi.service.metatype.annotations.Designate;
@@ -26,7 +19,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.amitinside.featureflags.example.ExampleFeatureFlag.MyConfig;
-import com.amitinside.featureflags.util.Configurable;
 
 @Designate(ocd = MyConfig.class)
 @Component(name = "ExampleFeatureFlag", immediate = true)
@@ -35,8 +27,6 @@ public final class ExampleFeatureFlag {
     private final Logger logger = LoggerFactory.getLogger(ExampleFeatureFlag.class);
 
     private volatile MyConfig config;
-    private ScheduledFuture<?> future;
-    private final ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
 
     @ObjectClassDefinition(id = "feature.flag.example")
     @interface MyConfig {
@@ -45,35 +35,24 @@ public final class ExampleFeatureFlag {
     }
 
     @Activate
-    protected void activate(final Map<String, Object> properties) {
-        modified(properties);
-        doPeriodicStuff();
+    protected void activate(final MyConfig config) {
+        modified(config);
     }
 
     @Modified
-    protected void modified(final Map<String, Object> properties) {
-        config = Configurable.createConfigurable(MyConfig.class, properties);
+    protected void modified(final MyConfig config) {
+        this.config = config;
+        doStuff();
     }
 
-    @Deactivate
-    protected void deactivate() {
-        if (future != null) {
-            future.cancel(true);
-            future = null;
+    private void doStuff() {
+        if (config.osgi_feature_myfeature()) {
+            System.out.println("Example Feature Config >>Enabled<<");
+            logger.debug("Example Feature Config >>Enabled<<");
+            return;
+        } else {
+            System.out.println("Example Feature Config >>Disabled<<");
+            logger.debug("Example Feature Config >>Disabled<<");
         }
     }
-
-    private void doPeriodicStuff() {
-        future = executor.scheduleWithFixedDelay(() -> {
-            if (config.osgi_feature_myfeature()) {
-                System.out.println("Example Feature Config >>Enabled<<");
-                logger.debug("Example Feature Config >>Enabled<<");
-                return;
-            } else {
-                System.out.println("Example Feature Config >>Disabled<<");
-                logger.debug("Example Feature Config >>Disabled<<");
-            }
-        }, 0, 15, TimeUnit.SECONDS);
-    }
-
 }
