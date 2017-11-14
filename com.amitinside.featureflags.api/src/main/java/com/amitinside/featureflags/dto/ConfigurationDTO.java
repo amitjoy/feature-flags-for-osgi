f8c31b3459b897da1433ca01903043d78f2ddcaf
Amit Kumar Mondal <admin@amitinside.com>
2017-11-14 15:04:33 +0100

    Detailed Javadoc for DTOs

--- com.amitinside.featureflags.api/src/main/java/com/amitinside/featureflags/dto/ConfigurationDTO.java
+++ com.amitinside.featureflags.api/src/main/java/com/amitinside/featureflags/dto/ConfigurationDTO.java
@@ -16,13 +16,22 @@ import org.osgi.annotation.versioning.ProviderType;
 import com.amitinside.featureflags.FeatureManager;
 
 /**
+ * Data Transfer Object for a configuration comprising features.
+ *
+ * <p>
  * A feature configuration is defined by its associated configuration PID and list
  * of associated features.
+ * </p>
+ *
  * <p>
- * A feature is specified in OSGi configurations with a name format of
- * {@code osgi.feature.X} where X is the name of your feature. An OSGi configuration
- * is considered to be a feature specific configuration if it comprises features as
- * aforementioned.
+ * A feature is specified in OSGi configuration with a name format of
+ * {@code osgi.feature.X} where X is the name of your feature and {@code osgi.feature.}
+ * ({@link FeatureManager#FEATURE_NAME_PREFIX}) is a standard prefix to specify the
+ * features.
+ * <br/>
+ * <br/>
+ * An OSGi configuration is considered to be a feature specific configuration
+ * if it comprises features as aforementioned.
  * </p>
  *
  * @noextend This class is not intended to be extended by consumers.
--- com.amitinside.featureflags.api/src/main/java/com/amitinside/featureflags/dto/FeatureDTO.java
+++ com.amitinside.featureflags.api/src/main/java/com/amitinside/featureflags/dto/FeatureDTO.java
@@ -14,11 +14,18 @@ import org.osgi.annotation.versioning.ProviderType;
 import com.amitinside.featureflags.FeatureManager;
 
 /**
+ * Data Transfer Object for a feature.
+ *
+ * <p>
  * A feature is defined by its name, description and flag denoting whether
  * the feature is enabled.
+ * </p>
+ *
  * <p>
- * A feature is specified in OSGi configurations with a name format of
- * {@code osgi.feature.X} where X is the name of your feature.
+ * A feature is specified in OSGi configuration with a name format of
+ * {@code osgi.feature.X} where X is the name of your feature and {@code osgi.feature.}
+ * ({@link FeatureManager#FEATURE_NAME_PREFIX}) is a standard prefix to specify the
+ * features.
  * </p>
  *
  * @noextend This class is not intended to be extended by consumers.
