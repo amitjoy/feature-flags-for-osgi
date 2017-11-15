508338560ef89649566cdf28a7f1c2827e12dd17
Amit Kumar Mondal <admin@amitinside.com>
2017-11-15 14:08:34 +0100

    Package Infos are updated with comprehensive javadoc

--- com.amitinside.featureflags.api/src/main/java/com/amitinside/featureflags/dto/package-info.java
+++ com.amitinside.featureflags.api/src/main/java/com/amitinside/featureflags/dto/package-info.java
@@ -10,7 +10,24 @@
 /**
  * Provides data transfer objects for feature flags instances
  *
+ * <p>
+ * Bundles wishing to use this package must list the package in the
+ * Import-Package header of the bundle's manifest. This package has two types of
+ * users: the consumers that use the API in this package and the providers that
+ * implement the API in this package.
+ *
+ * <p>
+ * Example import for consumers using the API in this package:
+ * <p>
+ * {@code  Import-Package: com.amitinside.featureflags.dto;version="[1.0,2.0)"}
+ * <p>
+ * Example import for providers implementing the API in this package:
+ * <p>
+ * {@code  Import-Package: com.amitinside.featureflags.dto;version="[1.0,1.1)"}
+ *
  * @since 1.0
  */
-@org.osgi.annotation.versioning.Version("1.0")
+@Version("1.0")
 package com.amitinside.featureflags.dto;
+
+import org.osgi.annotation.versioning.Version;
