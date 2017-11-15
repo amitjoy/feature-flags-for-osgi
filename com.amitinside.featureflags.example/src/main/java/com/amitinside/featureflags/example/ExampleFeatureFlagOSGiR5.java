77677b55d60916f85ead6473ef988cbe13143238
Amit Kumar Mondal <admin@amitinside.com>
2017-11-15 13:48:46 +0100

    AD Names are changed in Example Components

--- com.amitinside.featureflags.example/src/main/java/com/amitinside/featureflags/example/ExampleFeatureFlagOSGiR5.java
+++ com.amitinside.featureflags.example/src/main/java/com/amitinside/featureflags/example/ExampleFeatureFlagOSGiR5.java
@@ -26,7 +26,7 @@ public final class ExampleFeatureFlagOSGiR5 {
 
     @ObjectClassDefinition(id = "feature.flag.example1")
     @interface MyConfig {
-        @AttributeDefinition(name = "osgi.feature.myfeature", description = "My Feature Description")
+        @AttributeDefinition(name = "MyFunnyFeature", description = "My Feature Description")
         boolean osgi_feature_myfeature() default true;
     }
 
